a232f71c78bd6be9900686277c270985b881b0ba
johnsargent-sophos <107049988+johnsargent-sophos@users.noreply.github.com>
2023-03-02 17:42:05 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,11 @@ def MyFunc(foo):
   
 
 MyFunc("World")
+
+# example code snippet py_vuln00: Arbitrary Code Execution:
+compute_user_input = input('\nType something here to compute: ')
+
+if not compute_user_input:
+	print ("No input")
+else:
+	print ("Result: ", eval(compute_user_input))
