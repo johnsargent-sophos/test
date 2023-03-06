171ff57206166ada97646542449cce7bc59f430f
John Sargent <john.sargent@sophos.com>
2023-03-06 11:42:53 +0000

    PR1

--- hello.py
+++ hello.py
@@ -4,15 +4,19 @@ print("Hello")
 
 
 def MyFunc(foo):
-  print(foo)
+    print(foo)
   
+
+def input():
+    # example code snippet py_vuln00: Arbitrary Code Execution:
+    compute_user_input = input('\nType something here to compute: ')
+
+    if not compute_user_input:
+            print ("No input")
+    else:
+            print ("Result: ", eval(compute_user_input))
+
 
 MyFunc("World")
 
-# example code snippet py_vuln00: Arbitrary Code Execution:
-compute_user_input = input('\nType something here to compute: ')
-
-if not compute_user_input:
-	print ("No input")
-else:
-	print ("Result: ", eval(compute_user_input))
+input()
